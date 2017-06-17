5c4c3d8daa40f9b250a1073a4da00a91a19ffdbd
mochacat <cat.r.mocha@gmail.com>
2017-06-17 01:43:11 -0700

    organize twitworker code into object methods and properties

--- utils/twitWorker.js
+++ utils/twitWorker.js
@@ -5,49 +5,80 @@ const Twitter = require('twitter')
 const config = require('../config')
 const hashtag = require('../models/Hashtag')
 
-const twitClient = new Twitter({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.access_token_key,
-  access_token_secret: config.access_token_secret
-})
+const twitStream = module.exports = function(){
+  this.twitClient = new Twitter({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token_key: config.access_token_key,
+    access_token_secret: config.access_token_secret
+  })
 
-//grab twitter keywords from rest api later
-const keywords = ['js']
+  let currentStream = null
+  this.setStream = function(stream){
+    currentStream = stream
+  }
+  this.getStream = function(){
+    return currentStream
+  }
+}
 
-//set stream to null to check if it is active
-let twitStream = null;
-
-//need to disconnect stream every time new keywords come
-function init(){
-  if (twitStream === null){
-   
-    console.log('Connecting to Twitter Streaming API')
-
-    twitStream = twitClient.stream(
-        'statuses/filter', 
-        { track: keywords.join(',') }
-    )
+//start stream connection for keywords
+twitStream.prototype.track = function(keywords){
+  let currentStream = this.getStream()
+  
+  if (currentStream === null){    
+    if (!keywords.length){
+      throw new Error('No hashtags to track')
+    }
     
-    twitStream.on('warning', res => {
+    let tags = keywords.join(',')
+
+    console.log(`Connecting to Twitter Streaming API for keywords: ${tags}`)
+
+    const stream = this.twitClient.stream(
+        'statuses/filter', 
+        { track: tags }
+    )
+
+    this.setStream(stream)
+    
+    currentStream = this.getStream()
+    
+    currentStream.on('warning', res => {
       console.log('Stall warning: ', res.warning.message)
     })
 
-    twitStream.on('data', tweet => {
+    currentStream.on('data', tweet => {
       console.log('New tweet:', tweet && tweet.text)
       //persist it
     })
 
-    twitStream.on('error', err => {
+    currentStream.on('error', err => {
       console.log('Error:', err.message)
     })
+  } 
+}
+
+//destroy the stream connection for keywords
+twitStream.prototype.destroy = function(){
+  let currentStream = this.getStream()
+
+  if (currentStream !== null){
+    console.log('Destroying Twitter stream...')
+    currentStream.destroy()
+    this.setStream(null)
   } else {
-    console.log('Destroying Twitter stream')
-    twitStream.destroy()
-    clearInterval(interval)
-    process.exit(0)
+    throw new Error('Stream does not exist so cannot destroy')
   }
 }
 
+//TODO dynamically generate hashtags from twitter rest api
+let hashtags = ['js', 'node']
+
+const newStream = new twitStream()
+newStream.track(hashtags)
+
 //set interval for now
-var interval = setInterval(init, 10000)
+var timeout = setTimeout(function(){
+  newStream.destroy()
+}, 10000)
